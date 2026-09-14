a228642790545858af520145fc7b48ed33348193
zxmstar <zxh2856613773@gmail.com>
2026-09-14 17:16:08 +0800

    允许 Compose Foundation 实验性手势 API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi"
+    }
+
     buildFeatures { compose = true }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
