f6229b686870cf940ae55bc5046acfc26ca7afe8
zxmstar <zxh2856613773@gmail.com>
2026-09-14 17:11:55 +0800

    修复 Kotlin 与 Java JVM Target 不一致

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,12 @@ android {
         versionName = "0.1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions { jvmTarget = "17" }
     buildFeatures { compose = true }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
